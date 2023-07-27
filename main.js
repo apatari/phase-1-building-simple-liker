c677f57fa88428eff7a073f0791dcd911b0a596b
Andy Patari <andy.patari@gmail.com>
2023-07-26 20:52:47 -0400

    working to spec

--- main.js
+++ main.js
@@ -4,7 +4,49 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const allHearts = document.getElementsByClassName('like-glyph')
+for(element of allHearts) {
+  element.addEventListener('click', (e) => handleClick(e))
+}
 
+
+function handleClick(e) {
+  /* let heartStatus = e.target.textContent
+ if (heartStatus === '♡') {
+  console.log('Empty')
+  e.target.textContent = '♥'
+ } */
+
+ mimicServerCall()
+ .then(msg => {
+    handleSuccess(msg, e)
+    
+ })
+ .catch(error => handleFail(error))
+
+}
+
+function handleSuccess(msg, e) {
+  console.log(msg)
+  if (e.target.textContent === EMPTY_HEART) {
+    e.target.textContent = FULL_HEART
+    e.target.className = 'activated-heart'
+  } else {
+    e.target.textContent = EMPTY_HEART
+    e.target.classList.remove('activated-heart')
+  }
+  
+}
+
+
+function handleFail(msg) {
+  console.log(msg)
+  const modal = document.querySelector('#modal')
+  modal.classList.remove('hidden')
+  document.getElementById('modal-message').innerText = msg
+  setTimeout(() => modal.classList.add('hidden'), 3000)
+  
+}
 
 
 //------------------------------------------------------------------------------
